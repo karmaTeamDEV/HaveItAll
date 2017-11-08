2ceda1f0eff5c345f151016ee27b130a92f13d3a
bamadeb <bamadebupadhya@gmail.com>
2017-11-08 10:50:40 +0530

    bamadeb : post

--- js/postsController.js
+++ js/postsController.js
@@ -137,8 +137,105 @@ if(Extension){
 
   }
 
-  $scope.timeSince = function(date) {  
+ //  function get_time_difference_php($created_time)
+ // {
+       
+ //     date_default_timezone_set('America/Los_Angeles');
+ //   // (UTC-08:00) Pacific Time (US/Canada)
+ //        $str = strtotime($created_time);
+ //        $today = strtotime(date('Y-m-d H:i:s'));
+ //        // It returns the time difference in Seconds...
+ //        $time_differnce = $today-$str;
 
+ //        // To Calculate the time difference in Years...
+ //        $years = 60*60*24*365;
+
+ //        // To Calculate the time difference in Months...
+ //        $months = 60*60*24*30;
+
+ //        // To Calculate the time difference in Days...
+ //        $days = 60*60*24;
+  
+ //        // To Calculate the time difference in Hours...
+ //        $hours = 60*60;
+
+ //        // To Calculate the time difference in Minutes...
+ //        $minutes = 60;
+
+ //        if(intval($time_differnce/$years) > 1)
+ //        {
+ //  //echo"i am in year";
+ //  echo date("jS F, Y", strtotime($created_time)); 
+ //           // echo intval($time_differnce/$years)." years ago";
+ //        }
+ //  else if(intval($time_differnce/$years) > 0)
+ //        {
+ //   //echo"i am in year1";
+ //            //echo intval($time_differnce/$years)." year ago";
+ //   echo date("jS F, Y", strtotime($created_time)); 
+ //        }
+ //  else if(intval($time_differnce/$months) > 1)
+ //        {
+ //   //echo"i am in month";
+ //           // echo intval($time_differnce/$months)." months ago";
+ //     echo date("jS F, Y", strtotime($created_time)); 
+ //        }
+ //  else if(intval(($time_differnce/$months)) > 0)
+ //        {
+ //  //echo"i am in month1";
+ //  echo date("jS F, Y", strtotime($created_time)); 
+ //           // echo intval(($time_differnce/$months))." month ago";
+ //        }
+ //  else if(intval(($time_differnce/$days)) > 1)
+ //        {
+     
+ //  echo date("jS F, Y", strtotime($created_time)); 
+ //        //   echo intval(($time_differnce/$days))." days ago";
+ //        }
+ //  else if (intval(($time_differnce/$days)) > 0) 
+ //        {
+  
+ //  echo date("jS F, Y", strtotime($created_time)); 
+             
+ //         //  echo intval(($time_differnce/$days))." day ago";
+ //        }
+ //  else if (intval(($time_differnce/$hours)) > 1) 
+ //        {
+ //  //echo"i am in hour";
+ //            echo intval(($time_differnce/$hours))." hours ago";
+        
+ //  }else if (intval(($time_differnce/$hours)) > 0) 
+ //        {
+ //  //echo"i am in hour1";
+  
+ //            echo intval(($time_differnce/$hours))." hour ago";
+ //        }
+ //  else if (intval(($time_differnce/$minutes)) > 1) 
+ //        {
+ //     echo intval(($time_differnce/$minutes))." minutes ago";
+          
+ //        }
+ //  else if (intval(($time_differnce/$minutes)) > 0) 
+ //        {
+ //  //echo"i am in minutes1";
+ //      echo intval(($time_differnce/$minutes))." minute ago";
+           
+ //        }
+ //  else if (intval(($time_differnce)) > 1) 
+ //        {
+ //  echo intval(($time_differnce))." seconds ago";
+          
+ //        }
+ //  else
+ //        {
+ //  //echo "else";
+  
+ //          echo  "few seconds ago";
+ //        }
+ //  }
+
+  $scope.timeSince = function(date) {  
+      date_default_timezone_set('America/Los_Angeles');
       var seconds = Math.floor((new Date() - date) / 1000);
       var interval = Math.floor(seconds / 31536000);
 
