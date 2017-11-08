de72e2668ade02b252fe6daaf70f2db89303d7f0
bamadeb <bamadebupadhya@gmail.com>
2017-11-08 11:13:03 +0530

    feeds datetime

--- api/application/controllers/API.php
+++ api/application/controllers/API.php
@@ -2946,14 +2946,14 @@ function  getgalleryList_get($id){
 			{	
 				$mydata = $this->API_model->get_all_feeds($postid);	
 				//echo '<pre>';print_r($mydata); 
-				// foreach ($mydata as $key => $value) {					 
-				// 	 $value['lastactivity']= $this->timeAgo($value['post_date']);
-				// 	 $myarray[] = $value;
-				// }
+				foreach ($mydata as $key => $value) {					 
+					 $value['lastactivity']= $this->get_time_difference_php($value['post_date']);
+					 $myarray[] = $value;
+				}
 				//echo '<pre>';print_r($myarray);exit;
-				if($mydata){
+				if($myarray){
 					//$success = array('status' => '1','message' => 'succes');				 				
-					$this->response($mydata, 200); // 200 being the HTTP response code
+					$this->response($myarray, 200); // 200 being the HTTP response code
 				}else{
 					$Error = array('status' => '0','message' => 'error');		
 					$this->response($Error, 200);
@@ -2967,6 +2967,104 @@ function  getgalleryList_get($id){
 			}
 
 	}
+
+	function get_time_difference_php($created_time)
+ {
+       
+     	date_default_timezone_set('America/Los_Angeles');
+     // (UTC-08:00) Pacific Time (US/Canada);
+ 		//date_default_timezone_set('US/Eastern');
+        $str = strtotime($created_time);
+        $today = strtotime(date('Y-m-d H:i:s'));
+        // It returns the time difference in Seconds...
+        $time_differnce = $today-$str;
+
+        // To Calculate the time difference in Years...
+        $years = 60*60*24*365;
+
+        // To Calculate the time difference in Months...
+        $months = 60*60*24*30;
+
+        // To Calculate the time difference in Days...
+        $days = 60*60*24;
+  
+        // To Calculate the time difference in Hours...
+        $hours = 60*60;
+
+        // To Calculate the time difference in Minutes...
+        $minutes = 60;
+
+        if(intval($time_differnce/$years) > 1)
+        {
+  //echo"i am in year";
+  return date("jS F, Y", strtotime($created_time)); 
+           // echo intval($time_differnce/$years)." years ago";
+        }
+  else if(intval($time_differnce/$years) > 0)
+        {
+   //echo"i am in year1";
+            //echo intval($time_differnce/$years)." year ago";
+   return date("jS F, Y", strtotime($created_time)); 
+        }
+  else if(intval($time_differnce/$months) > 1)
+        {
+   //echo"i am in month";
+           // echo intval($time_differnce/$months)." months ago";
+     return date("jS F, Y", strtotime($created_time)); 
+        }
+  else if(intval(($time_differnce/$months)) > 0)
+        {
+  //echo"i am in month1";
+  return date("jS F, Y", strtotime($created_time)); 
+           // echo intval(($time_differnce/$months))." month ago";
+        }
+  else if(intval(($time_differnce/$days)) > 1)
+        {
+     
+  return date("jS F, Y", strtotime($created_time)); 
+        //   echo intval(($time_differnce/$days))." days ago";
+        }
+  else if (intval(($time_differnce/$days)) > 0) 
+        {
+  
+  return date("jS F, Y", strtotime($created_time)); 
+             
+         //  echo intval(($time_differnce/$days))." day ago";
+        }
+  else if (intval(($time_differnce/$hours)) > 1) 
+        {
+  //echo"i am in hour";
+            return intval(($time_differnce/$hours))." hours ago";
+        
+  }else if (intval(($time_differnce/$hours)) > 0) 
+        {
+  //echo"i am in hour1";
+  
+            return intval(($time_differnce/$hours))." hour ago";
+        }
+  else if (intval(($time_differnce/$minutes)) > 1) 
+        {
+     return intval(($time_differnce/$minutes))." minutes ago";
+          
+        }
+  else if (intval(($time_differnce/$minutes)) > 0) 
+        {
+  //echo"i am in minutes1";
+      return intval(($time_differnce/$minutes))." minute ago";
+           
+        }
+  else if (intval(($time_differnce)) > 1) 
+        {
+  return intval(($time_differnce))." seconds ago";
+          
+        }
+  else
+        {
+  //echo "else";
+  
+          return  "few seconds ago";
+        }
+  }
 
 	function timeAgo($timestamp){
 		//date_default_timezone_set('Asia/Calcutta'); 
